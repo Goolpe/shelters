64566c61fab04a2931bcee394731862473cab8f2
Goolpe <gooolpe@gmail.com>
2019-05-19 04:25:41 +0300

    add avatar and listview to profile screen

--- lib/src/widgets/navigation/profile.dart
+++ lib/src/widgets/navigation/profile.dart
@@ -1,24 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class ProfileSh extends StatelessWidget {
+  final List<Map<String, String>> settingsList = [
+    {
+      'title' : 'Местоположение',
+      'trailing': 'Санкт-Петербург',
+      'tap': '/shelters'
+    },
+    {
+      'title' : 'Мои питомцы',
+      'trailing': '',
+      'tap': '/shelters'
+    },
+    {
+      'title' : 'О приложении',
+      'trailing': '',
+      'tap': '/shelters'
+    },
+    {
+      'title' : 'Выйти',
+      'trailing': '',
+      'tap': '/shelters'
+    }
+    ];
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Column(
         children: <Widget>[
-          Card(
-            child: Text('new')
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 150,
+                padding: EdgeInsets.only(bottom: 50),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        'https://image.freepik.com/free-vector/seamless-dogs-pattern_1284-3639.jpg'
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: ListTile()
+                )
+              ),
+              Positioned(
+                top: 50,
+                right: 20,
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.blue,
+                  child: Icon(MdiIcons.paw, size: 50, color: Colors.white),
+                )
+              ),
+              Positioned(
+                top: 120,
+                left: 15,
+                child: Text('HopeHey')
+              ),
+            ],
           ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.place),
-              title: Text('Указать местоположение'),
-            )
+          Column(
+            children: List.generate(settingsList.length, (int i) {
+              return Card(
+                child: ListTile(
+                  title: Text(settingsList[i]['title'], style: TextStyle(
+                    color: i == settingsList.length - 1
+                    ? Colors.red
+                    : Colors.black),
+                  ),
+                  trailing: Text(settingsList[i]['trailing']),
+                  onTap: () => Navigator.pushNamed(context, settingsList[i]['tap']),
+                ),
+              );
+            }),
           ),
-          ListTile(
-            title: Text('О приложении'),
-          )
         ],
       )
     );
