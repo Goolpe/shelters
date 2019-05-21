4edbd5850e640205e3d3b2e14222502064053523
Goolpe <gooolpe@gmail.com>
2019-05-22 00:06:08 +0300

    location and app bar

--- lib/src/components/profile/my_location.dart
+++ lib/src/components/profile/my_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shelters/src/components/common/map.dart';
 import 'package:shelters/src/widgets/widgets.dart';
 
 class MyLocationSh extends StatelessWidget{
@@ -6,13 +7,16 @@ class MyLocationSh extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBarSh(title: 'Местоположение'),
-      body: Center(
-        child: Text(
-          'Shelters',
-          style: Theme.of(context).textTheme.display1,
-        ),
+      appBar: CustomAppBarSh(
+        title: 'Местоположение',
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){},
+          )
+        ],
       ),
+      body: MapSh()
     );
   }
 }
